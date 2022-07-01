dcfc7af82a970b000ad63850cdd405d786691946
ellisrichardj <richard.ellis@apha.gov.uk>
2022-07-01 15:31:13 +0100

    Correcting output name

--- bin/buildmatrix.sh
+++ bin/buildmatrix.sh
@@ -4,5 +4,6 @@ set -eo pipefail
 
 snponlyfasta=$1
 clade=$2
+today=$3
 
-~/snp-dists/snp-dists -c $snponlyfasta > "$clade"_matrix.csv
+~/snp-dists/snp-dists -c $snponlyfasta > ${clade}_${today}_matrix.csv
