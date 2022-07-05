fec9123943907860596d52a219aa50acbe9dafe6
ellisrichardj <richard.ellis@apha.gov.uk>
2022-07-05 13:16:07 +0100

    More comments

--- bin/cleanNuniq.sh
+++ bin/cleanNuniq.sh
@@ -2,13 +2,14 @@
 
 set -eo pipefail
 
-concat=$1
-today=$2
+concat=$1 # concatenated csv input
+today=$2 # datestamp
 
 # Bash pipe which removes the comment lines from the csv, then sorts on sample 
 # name and 'Ncount', and where there are duplicate sample names only the entry 
 # with the lowest 'Ncount' is retained
 
 sed '/^#/d' $concat |
+    (head -n 1 && tail -n +2 | # retain the header
     sort -t ',' -k1,1 -k14,14 |
-    sort -u -t ',' -k1,1 > bTB_Allclean_${today}.csv
+    sort -u -t ',' -k1,1) > bTB_Allclean_${today}.csv
