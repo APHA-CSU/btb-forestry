37957fbfcc8bf2bb614b2beb18f2c8f15ea9392e
ellisrichardj <richard.ellis@apha.gov.uk>
2023-01-07 12:30:46 +0000

    Fix input params

--- bin/filterSamples.sh
+++ bin/filterSamples.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
+set -eo pipefail
+
 # Extract relevant fields from clade lists, remove pre-determined outliers and
 # filter on Ncount
 
 fulllist=$1
 clade=$2
-maxN=$3
-outlierList=$4
+today=$3
+maxN=$4
+outlierList=$5
 
 while IFS= read Sample
 do
     sed -i "/^$Sample/d" $fulllist
 done <$outlierList
 
-echo -e "Submission,Sample,GenomeCov,MeanDepth,pcMapped,group,Ncount,ResultLoc" > ${clade}_${params.today}_samplelist.csv
-awk -F, '$15 <= '$maxN' {print \$1","\$2","\$3","\$4","\$6","\$9","\$15","\$16}' $fulllist >> ${clade}_${params.today}_samplelist.csv
+echo -e "Submission,Sample,GenomeCov,MeanDepth,pcMapped,group,Ncount,ResultLoc" > ${clade}_${today}_samplelist.csv
+awk -F, '$15 <= '$maxN' {print \$1","\$2","\$3","\$4","\$6","\$9","\$15","\$16}' $fulllist >> ${clade}_${today}_samplelist.csv
