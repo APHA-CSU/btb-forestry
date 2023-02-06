0a3e76b80c167de29bd64789e51c2853eda2f9b0
ellisrichardj <richard.ellis@apha.gov.uk>
2023-02-06 13:17:51 +0000

    remove surplus columns

--- bin/filterMetadata.py
+++ bin/filterMetadata.py
@@ -10,20 +10,24 @@ from datetime import datetime, date
 def filter(metadata_csv):
 
     date_out = date.today().strftime('%d%b%y')
+
     metadata_df = pd.read_csv(metadata_csv)
     metadata_df['SampleName']=metadata_df['SampleName'].astype(object)
-    #sort and deduplicate the metadata
+    # sort and deduplicate the metadata
     metadata_df.sort_values('SampleName', kind='mergesort', inplace = True)
     metadata_df.sort_values('MovementId', kind='mergesort', inplace = True)
     metadata_df.drop_duplicates('SampleName', inplace = True)
     metadata_df.set_index('SampleName', inplace = True)
-    
+
+    # remove surplus columns
+    metadata_df.drop(metadata_df.iloc[:, 13:30], inplace = True, axis = 1)
+        
+    # write revised metadata file
     metadata_df.to_csv('sortedMetadata_{}.csv'.format(date_out))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('metadata_csv', help='path to AssignedWGSClade.csv')
-    #parser.add_argument('bovis_csv', help='path to Bovis.csv')
     
     args = parser.parse_args()
 
