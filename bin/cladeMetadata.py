c84f03ee6947029f97443f06466fb1956cf75f99
ellisrichardj <richard.ellis@apha.gov.uk>
2023-02-06 15:22:23 +0000

    extract metadata for each clade

---
+++ bin/cladeMetadata.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+import argparse
+from datetime import date
+
+def combine(sortedMetadata_csv, cladelist_csv, clade):
+
+    date_out = date.today().strftime('%d%b%y')
+
+    sortedMetadata_df = pd.read_csv(sortedMetadata_csv)
+    cladelist_df = pd.read_csv(cladelist_csv)
+
+    clademetadata_df = pd.merge(cladelist_df, sortedMetadata_df, on = 'Submission', how = 'left')
+    clademetadata_df.rename(columns = {'Sample': 'name'}, inplace = True)
+    clademetadata_df.set_index('Submission', inplace = True)
+
+    clademetadata_df.to_csv('{}_metadata_{}.csv'.format(clade, date_out))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('sortedMetadata_csv', help = 'path to sortedMetadata.csv')
+    parser.add_argument('cladelist_csv', help = 'path to clade list')
+    parser.add_argument('clade', help = 'clade identifier')
+        
+    args = parser.parse_args()
+
+    combine(**vars(args))
