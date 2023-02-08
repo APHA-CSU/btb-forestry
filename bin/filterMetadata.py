917c9bd83d41c1eba79b3a2f28242077f34e6623
ellisrichardj <richard.ellis@apha.gov.uk>
2023-02-08 11:01:35 +0000

    simplify sorting and deduplication

--- bin/filterMetadata.py
+++ bin/filterMetadata.py
@@ -11,16 +11,15 @@ def filter(metadata_csv):
     date_out = date.today().strftime('%d%b%y')
 
     metadata_df = pd.read_csv(metadata_csv)
-    metadata_df['SampleName']=metadata_df['SampleName'].astype(object)
+    metadata_df['SampleName'] = metadata_df['SampleName'].astype(object)
     # sort and deduplicate the metadata
-    metadata_df.sort_values('SampleName', kind='mergesort', inplace = True)
-    metadata_df.sort_values('MovementId', kind='mergesort', inplace = True)
-    metadata_df.drop_duplicates('SampleName', inplace = True)
-    metadata_df.rename(columns = {'SampleName': 'Submission'}, inplace = True)
-    metadata_df.set_index('Submission', inplace = True)
+    metadata_df.sort_values('MovementId', kind='mergesort', inplace=True)
+    metadata_df.drop_duplicates('SampleName', inplace=True)
+    metadata_df.rename(columns = {'SampleName': 'Submission'}, inplace=True)
+    metadata_df.set_index('Submission', inplace=True)
 
     # remove surplus columns
-    metadata_df.drop(metadata_df.iloc[:, 13:30], inplace = True, axis = 1)
+    metadata_df.drop(metadata_df.iloc[:, 13:30], inplace=True, axis=1)
         
     # write revised metadata file
     metadata_df.to_csv('sortedMetadata_{}.csv'.format(date_out))
