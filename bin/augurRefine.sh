70ea5cc5c6ecfd89c2746411793fe5dea2c25ed7
ellisrichardj <richard.ellis@apha.gov.uk>
2023-02-14 11:45:18 +0000

    add link to json format

--- bin/augurRefine.sh
+++ bin/augurRefine.sh
@@ -8,8 +8,11 @@ outGroup=$3
 inputfasta=$4
 inputtree=$5
 
-# This process roots the tree using the outgroup and exports branch lengths in json format.
+# This process roots the tree using the outgroup and exports branch lengths in 
+# json format: 
 # https://docs.nextstrain.org/projects/augur/en/stable/usage/cli/refine.html
+# The general format of .json files is given here: 
+# https://docs.nextstrain.org/projects/augur/en/stable/usage/json_format.html
 
 augur refine -a $inputfasta \
             -t $inputtree \
