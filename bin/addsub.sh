832f7bdf0740519f74eff8048dc0b207ea4d47f2
Nick Pestell <nicholas.pestell@apha.gov.uk>
2023-03-01 12:42:56 +0000

    extract submission routine

--- bin/addsub.sh
+++ bin/addsub.sh
@@ -9,20 +9,22 @@ set -eo pipefail
 file=$1
 while IFS= read -r line;
 do
+    echo $line
     sample=$(echo "$line" | awk -F, '/[A-Z]*-*[0-9]{2,2}-[0-9]{4,5}-[0-9]{2,2}*/ {print $1}');
     if [[ -z $sample ]];
         then subno=$(echo "$line" | awk -F, '{print $1}');
-        else
-            IFS=-, read -r -a array <<< $sample
-            if [[ ${array[0]} = [A-Z]* ]]; 
-                then fivedig=$(echo "${array[2]}" | sed 's/\b[0-9]\{4\}\b/0&/1')
-                    year=$(echo ${array[3]} | sed 's/^\(.\{2\}\).*$/\1/')
-                    subno=$(echo AF-${array[1]}-$fivedig-$year); 
-                else fivedig=$(echo "${array[1]}" | sed 's/\b[0-9]\{4\}\b/0&/1')
-                    year=$(echo ${array[2]} | sed 's/^\(.\{2\}\).*$/\1/')
-                    subno=$(echo AF-${array[0]}-$fivedig-$year);
-            fi
+    else
+        IFS=-, read -r -a array <<< $sample
+        if [[ ${array[0]} = [A-Z]* ]]; 
+            then fivedig=$(echo "${array[2]}" | sed 's/\b[0-9]\{4\}\b/0&/1')
+                year=$(echo ${array[3]} | sed 's/^\(.\{2\}\).*$/\1/')
+                subno=$(echo AF-${array[1]}-$fivedig-$year); 
+            else fivedig=$(echo "${array[1]}" | sed 's/\b[0-9]\{4\}\b/0&/1')
+                year=$(echo ${array[2]} | sed 's/^\(.\{2\}\).*$/\1/')
+                subno=$(echo AF-${array[0]}-$fivedig-$year);
         fi
+    fi
+    echo $sunbo
     echo -e "$subno,$line" >> withsub.csv
     
 done <"$file"
