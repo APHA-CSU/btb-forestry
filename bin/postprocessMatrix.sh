d4e8aa2885316f46fa96b94bc2893ce974910895
Nick Pestell <nicholas.pestell@apha.gov.uk>
2023-03-01 16:05:33 +0000

    better variable name

--- bin/postprocessMatrix.sh
+++ bin/postprocessMatrix.sh
@@ -4,12 +4,12 @@
 
 set -eo pipefail
 
-file=$1
+snp_matrix=$1
 clade=$2
 today=$3
 
 IFS=","
-read -ra samples <<< $(head -n 1 $file)
+read -ra samples <<< $(head -n 1 $snp_matrix)
 subno_array=()
 for sample in "${samples[@]}"
     do  
@@ -20,7 +20,7 @@ headers=${subno_array[*]}
 echo "$headers" >> post_process_matrix.csv
 cat post_process_matrix.csv
 i=0
-sed 1d $file | while IFS= read -r line;
+sed 1d $snp_matrix | while IFS= read -r line;
 do
     i=$i+1
     echo -e "${subno_array[$i]},$( echo $line | cut -d ',' -f1 --complement )" >> ${clade}_${today}_matrix.csv
