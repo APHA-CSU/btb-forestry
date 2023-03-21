a072544eabc72ddb1db3d1ce9a3be70564dd8ced
ellisrichardj <richard.ellis@apha.gov.uk>
2023-03-21 16:58:14 +0000

    capture movement summary in metadata

--- bin/filterMetadata.py
+++ bin/filterMetadata.py
@@ -14,6 +14,7 @@ def filter(metadata_csv):
 
     metadata_df = pd.read_csv(metadata_csv, dtype='object')
     metadata_df.set_index('Submission', inplace=True)
+    move_count = metadata_df.iloc[:, 20::5].notnull().sum(axis=1)
 
     # remove surplus columns
     metadata_df = metadata_df.iloc[:, 0:11]
@@ -23,10 +24,18 @@ def filter(metadata_csv):
     metadata_df['CPHH'].replace(' ', '', regex=True, inplace=True)
     metadata_df.replace(r'^\s*$', np.nan, regex=True, inplace=True)
     metadata_df.fillna('NA', inplace=True)
+    
+    # Indicate if there is a history of cattle movement (True/False)
+    metadata_df['PreviousMovement'] = ''
+    metadata_df.loc[(metadata_df['Loc0'] != metadata_df['CPH'], 'PreviousMovement')] = 'True'
+    metadata_df.loc[(metadata_df['Loc0'] == metadata_df['CPH'], 'PreviousMovement')] = 'False'
+    metadata_df.loc[(metadata_df['Loc0'] == 'NA', 'PreviousMovement')] = 'False'
+    metadata_df['MoveCount'] = move_count
+    print(move_count)
 
     # write revised metadata file
     metadata_df.to_csv('sortedMetadata_{}.csv'.format(date_out))
-
+    
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
