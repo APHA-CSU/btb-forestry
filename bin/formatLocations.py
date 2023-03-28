39c1b629963b8a943cc08372a9be507b0956efce
ellisrichardj <richard.ellis@apha.gov.uk>
2023-03-28 23:23:06 +0100

    update for testing

--- bin/formatLocations.py
+++ bin/formatLocations.py
@@ -10,8 +10,8 @@ def locationfix(location_csv, counties_csv):
     date_out = date.today().strftime('%d%b%y')
 
     location_df = pd.read_csv(location_csv, dtype='object')
-    location_df.insert(0, 'LocationType', 'CPH')
     location_df = location_df.iloc[:, 0:3]
+    location_df.insert(0, 'LocationType', 'CPH')
 
     counties_df = pd.read_csv(counties_csv, dtype='object', sep='\t')
 
