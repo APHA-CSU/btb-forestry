32944950677fa6c7e9b40fe2a41a0de890620d86
ellisrichardj <richard.ellis@apha.gov.uk>
2023-04-24 18:09:45 +0100

    remove as colorings defined in config file

--- bin/augurExport.sh
+++ bin/augurExport.sh
@@ -24,8 +24,6 @@ augur export v2 -t $nwktree \
             --metadata $metadata \
             --node-data $phylojson $ntmutsjson \
             --auspice-config $configjson \
-            --color-by-metadata Host CPH CPH_Type County \
-            RiskArea OutsideHomeRange PreviousMovement MoveCount\
             --lat-longs $locations \
             --panels tree map \
             --colors custom-colours.tsv \
