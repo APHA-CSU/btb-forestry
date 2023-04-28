cda0e45e41d86af18df574321c200b58a6d5a8aa
ellisrichardj <richard.ellis@apha.gov.uk>
2023-04-28 14:13:38 +0100

    update script to define input

--- bin/splitClades.sh
+++ bin/splitClades.sh
@@ -2,11 +2,13 @@
 
 set -eo pipefail
 
+clean=$1
+
 # Separate sample list based on clade and outcome
-awk -F, '{print >> (\$9"_"\$7".csv")}' clean.csv
+awk -F, '{print >> (\$9"_"\$7".csv")}' $clean
 
 # Generate list of files containing low quality samples
 lowqual=$(ls *.csv | grep -v _Pass | grep -v clean)
 
 # Concatenate low quality samples into single file
-cat $lowqaul > LowQual.csv
+cat $lowqaul > lowQual.csv
