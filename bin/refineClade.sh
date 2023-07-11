65e912cf96b3666b5c8f581e7847674a026d1fd7
ellisrichardj <richard.ellis@apha.gov.uk>
2023-07-11 15:19:11 +0100

    Use python script for fasta filtering

--- bin/refineClade.sh
+++ bin/refineClade.sh
@@ -8,12 +8,15 @@ dropList=$3
 outGroup=$4
 outGroupLoc=$5
 
-awk -vRS=">" -vORS="" -vFS="\n" -vOFS="\n" 'NR>1 && $1!~/'$dropped'/ {print ">"$0}' $multifasta > refined.fas
+# Run python script to remove entries from multifasta
+refineClade.py ${multifasta} ${dropList} ${clade}
 
 # Add outgroup fasta (outgroup is predetermined for each clade)
 aws s3 cp "${outGroupLoc}consensus/${outGroup}_consensus.fas" "${outGroup}_consensus.fas"
 
-cat refined.fas "${outGroup}_consensus.fas" > refined_out.fas
+cat ${clade}_refined.fas "${outGroup}_consensus.fas" > refined_out.fas
 
 # Run snp-sites on refined data
-snp-sites -c -o ${clade}_${today}_refined_snp-only.fas
+snp-sites -c -o ${clade}_refined_snp-only.fas refined_out.fas
+
+rm 
