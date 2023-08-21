8858c20164e2a962b194ba24a92b0061e0901eaf
ellisrichardj <richard.ellis@apha.gov.uk>
2023-08-21 15:07:27 +0100

    fix s3 path

--- bin/s3prod.sh
+++ bin/s3prod.sh
@@ -8,12 +8,12 @@ S3_PATH=$1
 # skip the backup stage if the prod/ folder is missing in $S3_PATH
 {
     # extract date of current production files from $S3_PATH
-    aws s3 cp ${S3_PATH}/btb-forest_prod/Metadata/metadata.json metadata_prod.json &&
+    aws s3 cp ${S3_PATH}btb-forest_prod/Metadata/metadata.json metadata_prod.json &&
     {
 	backup_date=($(sed -e 's/^"//' -e 's/"$//' <<< $(jq '.today' metadata_prod.json)))
 	backup_date=${backup_date[0]}
 
 	# backup current production files in $S3_PATH
-	aws s3 mv --recursive ${S3_PATH}/btb-forest_prod/ ${S3_PATH}/btb-forest_${backup_date}/ --acl bucket-owner-full-control
+	aws s3 sync ${S3_PATH}btb-forest_prod/ ${S3_PATH}btb-forest_${backup_date}/ --acl bucket-owner-full-control
     }
 }
