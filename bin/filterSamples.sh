d0f61c0813589e9bfb24e55c063c7331e3530f95
ellisrichardj <richard.ellis@apha.gov.uk>
2024-07-16 16:43:15 +0100

    add filters for mismatch and anomolous calls

--- bin/filterSamples.sh
+++ bin/filterSamples.sh
@@ -10,6 +10,7 @@ clade=$2
 today=$3
 maxN=$4
 outlierList=$5
+maxvar=3
 
 while IFS= read Sample
 do
@@ -17,7 +18,9 @@ do
 done <$outlierList
 
 echo -e "Submission,Sample,GenomeCov,MeanDepth,pcMapped,group,Ncount,ResultLoc" > ${clade}_${today}_samplelist.csv
-awk -F, '$15 <= '$maxN' {print $1","$2","$3","$4","$6","$9","$15","$16}' $fulllist >> ${clade}_${today}_samplelist.csv
+awk -F, '$15 <= '$maxN' && $12 <= '$maxvar' && $14 <= '$maxvar' {print $1","$2","$3","$4","$6","$9","$15","$16}' $fulllist >> ${clade}_${today}_samplelist.csv
+
+## $12 <= $maxvar && $14 <= $maxvar #(mismatch and and anomolous calls less than 2.5%)
 
 echo -e "Submission,Sample,GenomeCov,MeanDepth,pcMapped,group,Ncount,ResultLoc" > ${clade}_${today}_highN.csv
 awk -F, '$15 > '$maxN' {print $1","$2","$3","$4","$6","$9","$15","$16}' $fulllist >> ${clade}_${today}_highN.csv
