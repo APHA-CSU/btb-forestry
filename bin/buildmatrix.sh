758d4ffb5e4d49914e07c66e23ad93c83f3e302e
ellisrichardj <richard.ellis@apha.gov.uk>
2024-07-24 17:51:25 +0100

    add matrix stats to buildmatrix script

--- bin/buildmatrix.sh
+++ bin/buildmatrix.sh
@@ -5,8 +5,12 @@ set -eo pipefail
 snponlyfasta=$1
 clade=$2
 today=$3
+maxN=$4
+outGroup=$5
 
 # Runs snp-dists (https://github.com/tseemann/snp-dists) to generate distance 
 # matrix using output from snp-sites
 
 ~/snp-dists/snp-dists -c $snponlyfasta > ${clade}_${today}_matrix.csv
+
+$projectDir/bin/matrixStats.py ${clade}_${today}_matrix.csv ${maxN} ${outGroup} ${clade}
