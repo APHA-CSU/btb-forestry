508861ca0695e67ad690b153a277a0358c87dcca
ellisrichardj <richard.ellis@apha.gov.uk>
2025-09-12 11:06:54 +0100

    add TODO for retain Pass samples

--- bin/cleanNuniq.sh
+++ bin/cleanNuniq.sh
@@ -9,6 +9,9 @@ today=$2 # datestamp
 # name and 'Ncount', and where there are duplicate sample names only the entry 
 # with the lowest 'Ncount' is retained
 
+#TODO Need to add function to ensure that Pass samples are retained over non-Pass samples
+# even if they have a higher Ncount
+
 head -n 1 $concat > bTB_Allclean_${today}.csv && tail -n +2 $concat | # retain the header
     (sort -t ',' -k1,1 -n -k16,16 | # simple sort on colums containing submission and Ncount
     sort -u -t ',' -k1,1 |
